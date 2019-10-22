c026b7020fce33252798a4b3b493a72f286b8b8a
rinkky <rinkky@live.com>
2019-10-22 17:45:59 +0800

    update version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='android-emulator-manager',
-    version='1.0.2',
+    version='1.0.3',
     description='android emulator manager',
     author='rinkky',
     packages=['android_emulator_manager']
