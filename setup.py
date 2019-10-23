3680def5a4309ff2c87a31393db939f7f665d617
ricky <rinkky@live.com>
2019-10-23 10:19:20 +0800

    update version: 1.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='android-emulator-manager',
-    version='1.0.3',
+    version='1.0.4',
     description='android emulator manager',
     author='rinkky',
     packages=['android_emulator_manager']
