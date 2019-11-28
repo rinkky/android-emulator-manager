44852a20194b14f6a159f841edbcb1420a2536cc
ricky <rinkky@live.com>
2019-11-28 11:24:37 +0800

    add docstring

--- android_emulator_manager/emulator_pool.py
+++ android_emulator_manager/emulator_pool.py
@@ -1,3 +1,11 @@
+"""
+Emulator pool.
+
+Devices info is configured in a yaml file.
+
+Environment variables `EMULATOR_POOL_CONFIG` define the config file path.
+"""
+
 import os
 import re
 
@@ -8,7 +16,24 @@ EMULATOR_POOL_CONFIG_ENV = 'EMULATOR_POOL_CONFIG'
 pool_cfg_file = os.environ[EMULATOR_POOL_CONFIG_ENV]
 
 def get_all():
-    """get all devices that is not turned on"""
+    """get all devices that is not turned on
+    
+    returns:
+    ```
+        {
+            'Android6.0': {
+                'id': 'emulator-5554',
+                'port': 5554,
+                'name': 'Android6.0',
+                'version': '6.0'
+            },
+            'Android7.0' : {
+                ...
+            },
+            ...
+        }
+    ```
+    """
     with open(pool_cfg_file) as file:
         cfg = YAML().load(file)
     devices = list(cfg['devices'].values())
@@ -19,7 +44,18 @@ def get_all():
     return off_devices
 
 def get_one():
-    """get a device which is not turned on"""
+    """get a device which is not turned on
+    
+    returns:
+    ```
+        {
+            'id': 'emulator-5554',
+            'port': 5554,
+            'name': 'Android6.0',
+            'version': '6.0'
+        }
+    ```
+    """
     with open(pool_cfg_file) as file:
         cfg = YAML().load(file)
     devices = list(cfg['devices'].values())
